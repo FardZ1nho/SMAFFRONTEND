36a5c91de6827f066c72648e9e345aaf5a12136f
FardZ1nho <147080871+FardZ1nho@users.noreply.github.com>
2026-01-15 14:20:30 -0500

    Act Environment

--- src/app/services/proveedor-service.ts
+++ src/app/services/proveedor-service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Proveedor } from '../models/proveedor';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'

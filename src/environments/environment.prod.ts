e9597505c435c92187ec9e80c6e9808ec08b8e0c
FardZ1nho <147080871+FardZ1nho@users.noreply.github.com>
2026-01-16 20:35:09 -0500

    environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
+// src/environments/environment.ts
 export const environment = {
-  production: true,
-  base: 'https://localhost:8080'
+  production: true,  // ⭐ Cambiar a true
+  base: 'https://smafbackend.onrender.com'  // ⭐ Cambiar a la URL de producción
 }

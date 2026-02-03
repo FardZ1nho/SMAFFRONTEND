3f6bacc9444ea026b66c46f0da88ac62d94bcec0
FardZ1nho <147080871+FardZ1nho@users.noreply.github.com>
2026-02-03 11:42:52 -0500

    envfc

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-// 1. IMPORTAR ANIMACIONES (Necesario para que el calendario se abra suavemente)
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
-// 2. IMPORTAR ADAPTADORES DE FECHA
+// 1. CAMBIO IMPORTANTE: Usamos 'provideNoopAnimations'
+// Esto permite que el calendario funcione pero SIN efectos de movimiento (evita el error de build)
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
+
 import { provideNativeDateAdapter, MAT_DATE_LOCALE } from '@angular/material/core';
 
 import { routes } from './app.routes';
@@ -25,9 +26,10 @@ export const appConfig: ApplicationConfig = {
     ),
     provideCharts(withDefaultRegisterables()),
 
-    // ✅ 3. AGREGADOS AQUÍ:
-    provideAnimationsAsync(),        // Habilita animaciones (modales, datepickers)
-    provideNativeDateAdapter(),      // Habilita el manejo de fechas nativo
-    { provide: MAT_DATE_LOCALE, useValue: 'es-PE' } // Pone el calendario en Español Perú
+    // ✅ AQUÍ ESTÁ EL ARREGLO:
+    provideNoopAnimations(), // Reemplaza a provideAnimationsAsync()
+
+    provideNativeDateAdapter(),
+    { provide: MAT_DATE_LOCALE, useValue: 'es-PE' } 
   ]
 };
